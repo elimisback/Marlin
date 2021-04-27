d22fa458f24aefd4596ac0e1e15315161a5e5313
elimisback <elimisback@gmail.com>
2021-04-26 20:26:59 -0500

    undo linear advance for now

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1048,7 +1048,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35.8, 0, -1.6 }
+#define NOZZLE_TO_PROBE_OFFSET { 35.8, 0, -1.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
