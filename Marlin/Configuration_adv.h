7cf7feb5e9b20c2c90a9c022f0430056a7c7db90
elimisback <elimisback@gmail.com>
2022-02-27 11:18:16 -0600

    Limit adjust
    Raising the limit

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2719,7 +2719,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       800
+    #define Z_CURRENT       600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.075
@@ -2729,7 +2729,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      800
+    #define Z2_CURRENT      600
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.075
