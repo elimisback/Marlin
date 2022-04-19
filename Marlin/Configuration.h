72c69939aed8265b2868c596802bc14f7527a92d
elimisback <elimisback@gmail.com>
2022-04-19 08:08:02 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3114,3 +3114,10 @@
 
 // Disable servo with M282 to reduce power consumption, noise, and heat when not in use
 //#define SERVO_DETACH_GCODE
+
+//Custom Defines
+#define DIAG_JUMPERS_REMOVED
+
+#define I2C_EEPROM
+#define MARLIN_EEPROM_SIZE              0x8000
+
