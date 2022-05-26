35cdb5ab87af263c5bd1169b0ae5ff956e235d91
elimisback <elimisback@gmail.com>
2022-05-26 07:21:18 -0500

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4492,4 +4492,4 @@
 //#define SOFT_RESET_ON_KILL            // Use a digital button to soft-reset the controller after KILL
 
 // Report uncleaned reset reason from register r2 instead of MCUSR. Supported by Optiboot on AVR.
-//#define OPTIBOOT_RESET_REASON 
+//#define OPTIBOOT_RESET_REASON
